746158448e317299d81738f1124e2be831eee172
AlejRes <alejrespal@gmail.com>
2022-09-26 14:32:00 +0200

    first commit

---
+++ src/com/alejrest/Main.java
@@ -0,0 +1,29 @@
+package com.alejrest;
+
+public class Main {
+    public static void main(String[] args) {
+        byte num1 = 1;
+        short num2 = 2;
+        int num3 = 3;
+        long num4 = 4L;
+        double num5 = 5.5d;
+        float num6 = 6.6f;
+
+        char  caracter = 'a';
+        String cadena = "Hola openbootcamp";
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        System.out.println("Variable de tipo byte " + num1);
+        System.out.println("Variable de tipo short " + num2);
+        System.out.println("Variable de tipo int " + num3);
+        System.out.println("Variable de tipo long " + num4);
+        System.out.println("Variable de tipo double " + num5);
+        System.out.println("Variable de tipo float " + num6);
+        System.out.println("Variable de tipo char " + caracter);
+        System.out.println("Variable String " + cadena);
+        System.out.println("Variable de tipo boolean con valor " + verdadero);
+        System.out.println("Variable de tipo boolean con valor " + falso);
+    }
+}
